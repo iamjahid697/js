0dbf1046627758d90bde688dbddaa4f4a314c32f
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-16 17:18:01 +0600

    three type of variable added

--- variable.js
+++ variable.js
@@ -1,3 +1,5 @@
+// number variable
+
 var bananaPrice = 12;
 console.log(bananaPrice)
 
@@ -6,3 +8,20 @@ console.log(myRoll);
 
 var currentYear = 2021;
 console.log(currentYear);
+
+// string variable
+
+var myName = "Md Jahidul Islam";
+console.log(myName);
+
+var myUniversityName = "Dhaka City Collage";
+console.log(myUniversityName);
+
+var profession = "Student";
+console.log(profession);
+
+// Boolean variable
+
+var isHot = true ;
+var isRich = false;
+console.log(isHot);
