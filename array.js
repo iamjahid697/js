a7cf3561dc38bc6e9f98250ed0c4330b8490b5ee
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-17 13:45:52 +0600

    array.js added

---
+++ array.js
@@ -0,0 +1,10 @@
+var friendsAge = [15,17,14,16];
+
+var sonaliAge = friendsAge[2];
+
+friendsAge[1] = 21;
+var position = friendsAge.indexOf(14);
+
+console.log(friendsAge)
+console.log(sonaliAge)
+console.log(position)
