fa91302657d04ad607e3599d8dd061da070ea7a7
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-25 00:05:25 +0600

    fibonacci.js added

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+function fibonacci(n){
+    var fibo = [0, 1];
+    for (var i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+
+    }
+    return fibo;
+}
+var result = fibonacci(13);
+console.log(result);
