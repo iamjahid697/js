f61b0c85fc5bfb6067360f08741cc8addeb39d61
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-24 12:33:33 +0600

    function added

--- for.js
+++ for.js
@@ -1,6 +1,5 @@
-var nums = {55,66,77,88,99,11,44};
+var nums = [55 , 66,77,88,99,11,44];
 
 for(var i = 0; i<nums.length;i++){
-    var element var element:number 
-    console.log(element)
+    console.log(i)
 }
--- function.js
+++ function.js
@@ -8,3 +8,25 @@ var date =14;
 var place ="Restaurent";
 sayLoveYou();
 var hangoutPlace = "Tsc";
+
+//doubleIt 
+
+function doubleIt(num) {
+    var result = num * 2;
+    return result;
+    
+}
+var first = doubleIt(5);
+var second = doubleIt(50);
+var total = first + second;
+console.log(total);
+
+//sum of two numbre
+
+
+function add (num1,num2){
+    var result =num1 + num2;
+    return result;
+}
+var sum = add(15,17);
+console.log(sum);
