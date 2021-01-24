225f7e87426ede4a37c0c4ae86a8859aec89bb18
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-24 13:05:15 +0600

    some changes

--- object.js
+++ object.js
@@ -7,5 +7,11 @@ var phoneNo =student.phone;
 // var phonePropName ="phone";
 // var phoneNo =student[phonePropName];
 
+/update phone number 
+
+student2.phone = 5345;
+student2["phone] = 697349;
+student2[phonePropName] = 99599;
+
 console.log(phoneNo);
 console.log(student2);
