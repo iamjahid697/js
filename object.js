2e45fee294f38a0728cc8e2612af9b2c386c77a1
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-24 12:50:52 +0600

    object.js added

---
+++ object.js
@@ -0,0 +1,11 @@
+var student = { id:121, phone:1745, name:"Jahid"};
+var student2 = { id:122, phone:1234, name:"Tetuta"};
+
+var phoneNo =student.phone;
+//var phoneNo = student["phone"];
+
+// var phonePropName ="phone";
+// var phoneNo =student[phonePropName];
+
+console.log(phoneNo);
+console.log(student2);
