097c09e6bc6e2cdce981490bca823b7cd8c752bc
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-24 22:45:10 +0600

    factorial in while loop added

--- factorial.js
+++ factorial.js
@@ -1,9 +1,25 @@
+//forloop factorial
+
 function factorial(n){
     var factorial = 1 ;
-    for(var i = 1; i<=n; i++){
+    for(var i = 1; i<= n; i++){
         factorial = factorial * i;
     }
     return factorial;
 }
-var result =factorial(5);
+var result =factorial(0);
+console.log(result);
+
+//while loop factorial
+
+function factorial(n){
+    var i = 1;
+    var factorial = 1;
+    while (i <= n) {
+        factorial = factorial * i;
+        i++;
+    }
+    return factorial;
+}
+var result = factorial(10);
 console.log(result);
