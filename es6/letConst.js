d63325f41037b10067a0636364915c0fb4d1c509
Md Jahidul Islam <iamjahid697@gmail.com>
2021-02-11 20:05:43 +0600

    added

--- es6/letConst.js
+++ es6/letConst.js
@@ -12,3 +12,9 @@ console.log(numbers);
 let patientName = "rahim Chacha";
 patientName = "Fatema Khala";
 console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++ ) {
+    sum = sum + i;
+}
+//console.log(i);
