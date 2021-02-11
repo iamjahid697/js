1c7096e6577c152c701578d7bea0d894e5c638e7
Md Jahidul Islam <iamjahid697@gmail.com>
2021-02-11 19:55:17 +0600

    added

--- es6/letConst.js
+++ es6/letConst.js
@@ -5,4 +5,10 @@ const numbers = [12, 45];
 numbers[1] = 88;
 numbers.push(12);
 
+const nayok = {name:"Salman Khan", phone:34359};
+
 console.log(numbers);
+
+let patientName = "rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
